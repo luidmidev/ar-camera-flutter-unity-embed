7dc3ecbc2c142af819cc0d878ac9ccf57868f604
playerluis <playerluis159@gmail.com>
2024-04-05 02:17:30 -0500

    add ios support persists a  log file

--- unity/AR Camera/Assets/Scripts/Log/FileLogger.cs
+++ unity/AR Camera/Assets/Scripts/Log/FileLogger.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.IO;
 using UnityEngine;
+using static UnityEngine.RuntimePlatform;
 
 namespace Log
 {
     public static class FileLogger
     {
-        private static string PathLog => Application.platform == RuntimePlatform.Android
+        private static string PathLog => Application.platform is Android or IPhonePlayer
             ? Application.persistentDataPath + "/log.txt"
             : Application.dataPath + "/log.txt";
 
